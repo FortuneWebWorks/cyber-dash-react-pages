16507bd875e082ce5fd27a7711cdee9a3f53e65f
lifeloop2180 <lifeloop2180@gmail.com>
2022-05-16 22:45:01 +0430

    :zap: Material Ui added and an input added  to spine component

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,4 +1,7 @@
 import { createUseStyles } from 'react-jss';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import NativeSelect from '@mui/material/NativeSelect';
 
 const useStyles = createUseStyles({
   conatiner: {
@@ -44,7 +47,24 @@ const MenuItem = ({ Icon, title, isActive }) => {
 
       {/* Side menue */}
       {isActive && title !== 'Dashboard' && (
-        <div className={styles.sideMenu}>Sub</div>
+        <div className={styles.sideMenu}>
+          <FormControl fullWidth>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+              Age
+            </InputLabel>
+            <NativeSelect
+              defaultValue={30}
+              inputProps={{
+                name: 'age',
+                id: 'uncontrolled-native',
+              }}
+            >
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+            </NativeSelect>
+          </FormControl>
+        </div>
       )}
     </div>
   );
